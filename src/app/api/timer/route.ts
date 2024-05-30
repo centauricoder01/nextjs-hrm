46ce931fd73ac4d624c7545de06eb43486c95d93
Rajendra <itsrajendrapatel@gmail.com>
2024-05-30 09:45:32 +0530

    Done with few changes and error

--- src/app/api/timer/route.ts
+++ src/app/api/timer/route.ts
@@ -34,7 +34,7 @@ export async function POST(request: Request) {
         }
         return NextResponse.json({
           success: true,
-          message: "started",
+          message: "Timer has been started started",
           responseBody: null,
         });
       case "break":
